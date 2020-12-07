dc8a3e8939ba2f962eb2117ffdfedc85344ea558
Akween <60363019+Akween@users.noreply.github.com>
2020-12-07 19:38:47 +0100

    Add files via upload

---
+++ eggstercog/__init__.py
@@ -0,0 +1,5 @@
+from .eggstercog import Eggstercog
+
+
+def setup(bot):
+    bot.add_cog(Eggstercog())
---
+++ eggstercog/eggstercog.py
@@ -0,0 +1,7 @@
+from redbot.core import commands
+
+class Eggstercog(commands.Cog):
+
+    @commands.command()
+    async def yeetus(self, ctx):
+        await ctx.send("yeetus yeetus what is this deletus")
