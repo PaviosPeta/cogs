f30da0506acbc0b892e19bda451b0d61fb135475
PaviosPeta <75677982+PaviosPeta@users.noreply.github.com>
2020-12-08 15:29:05 +0100

    Update eggstercog.py

--- eggstercog/eggstercog.py
+++ eggstercog/eggstercog.py
@@ -87,7 +87,7 @@ class Eggstercog(commands.Cog):
 
             
     @commands.command()
-    async def stop("""self, ctx"""): 
+    async def stop(self, ctx): 
         buul = False
         await ctx.send("it is wrong now")
 #        return buul
